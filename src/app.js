5dca504ab93d56a819dcd145d7d5009195165fef
ahmadijmail <ahmadijmil2010@gmail.com>
2022-08-30 04:09:15 +0300

    add

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Login from "./components/auth/login.js";
 
 import ToDo from "./components/todo/todo.js";
 import { Settings } from "./conText/settings.js";
-
+import LoginProvider from "./components/auth/context"
+//import Login from "./components/auth/auth"
 export default function App() {
   return (
+
+    
     <Settings>
+      <LoginProvider>
+      <Login/>
+      </LoginProvider>
       <ToDo />
     </Settings>
   );
