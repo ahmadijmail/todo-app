25abe48ffa93882fdbfc1debca6a66c44c865b2a
ahmadijmail <ahmadijmil2010@gmail.com>
2022-08-31 03:56:14 +0300

    lab 33&44 done

--- src/components/auth/context.js
+++ src/components/auth/context.js
@@ -1,87 +1,83 @@
 import React, { useState, useEffect } from "react";
-import superagent from 'superagent';
-import base64 from 'base-64';
-import jwt from 'jsonwebtoken';
-import cookie from 'react-cookies';
-
+import superagent from "superagent";
+import base64 from "base-64";
+import jwt from "jsonwebtoken";
+export const SettingsContext = React.createContext();
 export const LoginContext = React.createContext();
-const API = `https://auth-api-ahmadijmail.herokuapp.com/signin`
+const API = `https://auth-api-ahmadijmail.herokuapp.com/signin`;
 export default function LoginProvider(props) {
+  const [loginStatus, setLoginStatus] = useState(false);
 
-    const [loginStatus, setLoginStatus] = useState(false);
-    const [usernameFromcookie, setuser2]=useState()
-    const [user, setUser] = useState({
-        username: cookie.load('username') || "",
-        capabilities: cookie.load('capabilities') || []
-    });
+  const [user, setUser] = useState({
+    username: JSON.parse(localStorage.getItem("username")) || "",
+    capabilities: JSON.parse(localStorage.getItem("capabilities")) || [],
+  });
+const [error,setError]=useState()
+  useEffect(() => {
+    const tokenFromCookies = JSON.parse(localStorage.getItem("token"));
+    if (tokenFromCookies) {
+      setLoginStatus(true);
 
+      setUser(user);
+    } else {
+      setLoginStatus(false);
+      // setUser({});
+    }
+  }, []);
+  const loginFunction = async (username, password) => {
+    try {
+      const response = await superagent
+        .post(`${API}`)
+        .set(
+          "authorization",
+          `Basic ${base64.encode(`${username}:${password}`)}`
+        );
 
-    useEffect(() => {
-        const tokenFromCookies = cookie.load('token');
-        if (tokenFromCookies) {
-            setLoginStatus(true);
-            setUser(user);
-           
-        } else {
-            setLoginStatus(false);
-            setUser({})
-        }
+      validateMyUser(response.body.user);
 
-        const username= cookie.load('username')
-        setuser2(username)
-    }, []);
-    const loginFunction = async (username, password) => {
-        try {
-            const response = await superagent.post(`${API}`).set('authorization', `Basic ${base64.encode(`${username}:${password}`)}`);
-            console.log('body >>> ', response.body);
-            validateMyUser(response.body);
-        } catch (err) {
+      setUser(response.body.user);
+    } catch (err) {
+        setError(err)
+        console.log(err);
+    }
+  };
+  const logoutFunction = () => {
+    setLoginStatus(false);
+    window.location.reload();
+    setUser({});
+    localStorage.clear("token");
+    localStorage.clear("actions");
+    localStorage.clear("username");
+  };
 
-        }
+  const validateMyUser = (user) => {
+    if (user.token) {
+      const userFromToken = jwt.decode(user.token);
+
+      setLoginStatus(true);
+      //setUser(user);
+      localStorage.setItem("token", JSON.stringify(user.token));
+
+      localStorage.setItem("username", JSON.stringify(user.username));
+
+      localStorage.setItem("capabilities", JSON.stringify(user.capabilities));
+    } else {
+      setLoginStatus(false);
+      setUser({});
     }
-    const logoutFunction = () => {
-        setLoginStatus(false);
-        setUser({});
-        cookie.remove('token');
-        cookie.remove('actions');
-        cookie.remove('username');
-    }
-    const validateMyUser = (user) => {
-        if (user.token) {
-            const userFromToken = jwt.decode(user.token);
-            console.log('username >>>> ', userFromToken);
-            setLoginStatus(true);
-            setUser(user);
-            console.log(user);
-            cookie.save('token', user.user.token);
-           
-            cookie.save('username', user.user.username);
-           
-         
-            cookie.save('capabilities', user.user.capabilities)
-        } else {
-            setLoginStatus(false);
-            setUser({});
-        }
-    }
-    //read
-    const can = (action) => {
-   
-        return user?.capabilities?.includes(action);
-    }
-    const state = {
-        loginStatus: loginStatus,
-        loginFunction: loginFunction,
-        logoutFunction: logoutFunction,
-        user: user,
-        usernameFromcookie:usernameFromcookie,
-       
-        canDo: can
-   
-    }
-    return (
-        <LoginContext.Provider value={state}>
-            {props.children}
-        </LoginContext.Provider>
-    )
+  };
+
+  const state = {
+    loginStatus: loginStatus,
+    loginFunction: loginFunction,
+    logoutFunction: logoutFunction,
+    user: user,
+    error:error,
+
+  };
+  return (
+    <LoginContext.Provider value={state}>
+      {props.children}
+    </LoginContext.Provider>
+  );
 }
